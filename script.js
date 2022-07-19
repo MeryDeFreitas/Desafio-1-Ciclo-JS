04390c4723f7c560cb8fdaf9e432b54cda9ac922
Mery De Freitas <67561739+MeryDeFreitas@users.noreply.github.com>
2022-07-19 18:07:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+for (let i = 1; i<=5; i++){
+    let numero1 = parseInt(prompt("Ingresar primer número"))
+    let numero2 = parseInt(prompt("Ingresar segundo número"))
+    let resultado = numero1 + numero2
+        if(i ==0){
+            break
+        }
+    alert("Tu resultado es " + resultado)
+    console.log("tu resultado es " +resultado)
+}
